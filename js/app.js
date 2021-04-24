0bd6a3cfbc2979121503dcc576369439d0deb8e3
Rallanvila <r.allanvila@gmail.com>
2021-04-23 21:48:48 -0400

    created updateInputsState function

--- js/app.js
+++ js/app.js
@@ -1,7 +1,8 @@
+//remove characters from numbers
 let getNumber = (str) =>{
     return Number(str.replace(/[^0-9\.-]+/g,""))
 }
-
+//setting the state of form
 let state = {
     price: getNumber(document.querySelectorAll('[name="price"]')[0].value),
     loan_years: document.querySelectorAll('[name="loan_years"]')[0].value,
@@ -11,43 +12,7 @@ let state = {
     home_insurance: document.querySelectorAll('[name="home_insurance"]')[0].value,
     hoa: document.querySelectorAll('[name="hoa"]')[0].value,
 }
-
-var ctx = document.getElementById('myChart').getContext('2d');
-var myChart = new Chart(ctx, {
-    type: 'bar',
-    data: {
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-        datasets: [{
-            label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
-            ],
-            borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
-            borderWidth: 1
-        }]
-    },
-    options: {
-        scales: {
-            y: {
-                beginAtZero: true
-            }
-        }
-    }
-});
-
+//setting variables
 let totalLoan,
 totalMonths,
 monthlyInterest,
@@ -57,12 +22,12 @@ monthlyHomeInsurance,
 monthlyHOA,
 labels = ["Principal & Interest", "Property Tax", "Home Insurance", "HOA"],
 backgroundColor= [
-    'rgba(255, 99, 132, 0.2)',
-    'rgba(54, 162, 235, 0.2)',
-    'rgba(255, 206, 86, 0.2)',
-    'rgba(75, 192, 192, 0.2)',
-    'rgba(153, 102, 255, 0.2)',
-    'rgba(255, 159, 64, 0.2)'
+    'rgba(255, 99, 132, 1)',
+    'rgba(54, 162, 235, 1)',
+    'rgba(255, 206, 86, 1)',
+    'rgba(75, 192, 192, 1)',
+    'rgba(153, 102, 255, 1)',
+    'rgba(255, 159, 64, 1)'
 ];
 borderColor= [
     'rgba(255, 99, 132, 1)',
@@ -72,6 +37,44 @@ borderColor= [
     'rgba(153, 102, 255, 1)',
     'rgba(255, 159, 64, 1)'
 ];
+//initialize chart.js
+let ctx = document.getElementById('myChart').getContext('2d');
+let myChart = new Chart(ctx, {
+    type: 'doughnut',
+    data: {
+        labels: labels,
+        datasets: [{
+            label: '# of Votes',
+            data: [
+                monthlyPrincipleInterest,
+                monthlyPropertyTaxes,
+                monthlyHomeInsurance,
+                monthlyHOA
+            ],
+            backgroundColor: backgroundColor,
+            borderColor: borderColor,
+            borderWidth: 1
+        }]
+    },
+});
+myChart.options.animation = false;
+//adding event listeners to inputs
+let updateInputsState = (event) =>{
+    let name = event.target.name;
+    let value = event.target.value;
+    if (name == 'price'){
+        value = getNumber(value);
+    }
+    state = {
+        ...state, 
+        [name]: value
+    }
+    console.log(state)
+}
 
+let i;
+let inputText = document.getElementsByClassName('form-group__textInput');
+for (i = 0; i < inputText.length; i++){
+    inputText[i].addEventListener('input', updateInputsState)
+}
 
-console.log(state)
